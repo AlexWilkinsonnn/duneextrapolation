d6fef30c2c0a9b1f9190ce1ca59d234262b1a265
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-06-20 09:46:53 -0500

    fixes to build

--- wc_scripts/prep_libtorch_cu1141_cudnn_sm60sm70.sh
+++ wc_scripts/prep_libtorch_cu1141_cudnn_sm60sm70.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load cuda11/11.1.4
+module load cuda11/11.4.1
 unsetup libtorch
 
 export LIBTORCH_INC=/work1/dune/users/awilkins/extrapolation/pytorch-install/include
@@ -8,6 +8,8 @@ export LIBTORCH_DIR=/work1/dune/users/awilkins/extrapolation/pytorch-install
 export LIBTORCH_FQ_DIR=/work1/dune/users/awilkins/extrapolation/pytorch-install
 export LIBTORCH_LIB=/work1/dune/users/awilkins/extrapolation/pytorch-install/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work1/dune/users/awilkins/extrapolation/pytorch-install/lib
+export Torch_DIR=/work1/dune/users/awilkins/extrapolation/pytorch-install 
+export LIBTORCH_VERSION=v1_6_0d # Need this so find_ups_product doesn't cry
 
 export CUDNN_LIBRARY=/work1/dune/users/awilkins/dsim/cudnn/lib64
 export CUDNN_INCLUDE_DIR=/work1/dune/users/awilkins/dsim/cudnn/include
