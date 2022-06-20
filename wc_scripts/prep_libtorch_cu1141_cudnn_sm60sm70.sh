c38fc176cef4750800458c432f3a6cdd82712863
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-06-20 09:17:15 -0500

    forgot to update paths

--- wc_scripts/prep_libtorch_cu1141_cudnn_sm60sm70.sh
+++ wc_scripts/prep_libtorch_cu1141_cudnn_sm60sm70.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-module load cuda11/11.1.1
+module load cuda11/11.1.4
 unsetup libtorch
 
-export LIBTORCH_INC=/work1/dune/users/awilkins/dsim/pytorch_cu1111_cudnn_sm70/pytorch-install/include
-export LIBTORCH_DIR=/work1/dune/users/awilkins/dsim/pytorch_cu1111_cudnn_sm70/pytorch-install
-export LIBTORCH_FQ_DIR=/work1/dune/users/awilkins/dsim/pytorch_cu1111_cudnn_sm70/pytorch-install
-export LIBTORCH_LIB=/work1/dune/users/awilkins/dsim/pytorch_cu1111_cudnn_sm70/pytorch-install/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work1/dune/users/awilkins/dsim/pytorch_cu1111_cudnn_sm70/pytorch-install/lib
+export LIBTORCH_INC=/work1/dune/users/awilkins/extrapolation/pytorch-install/include
+export LIBTORCH_DIR=/work1/dune/users/awilkins/extrapolation/pytorch-install
+export LIBTORCH_FQ_DIR=/work1/dune/users/awilkins/extrapolation/pytorch-install
+export LIBTORCH_LIB=/work1/dune/users/awilkins/extrapolation/pytorch-install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work1/dune/users/awilkins/extrapolation/pytorch-install/lib
 
 export CUDNN_LIBRARY=/work1/dune/users/awilkins/dsim/cudnn/lib64
 export CUDNN_INCLUDE_DIR=/work1/dune/users/awilkins/dsim/cudnn/include
