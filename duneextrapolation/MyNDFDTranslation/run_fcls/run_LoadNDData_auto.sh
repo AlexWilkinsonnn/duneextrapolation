b3803318f1e63bfc8efeaa3709fdb4cb175ec358
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-06-30 11:47:31 -0500

    script to make running the load data fcl more automated

---
+++ duneextrapolation/MyNDFDTranslation/run_fcls/run_LoadNDData_auto.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Script for running run_LoadNDData.fcl correct
+# Sets the number of events correctly and creates a temporary fcl to run over the desired file.
+
+trap '{ rm -f /tmp/run_LoadNDData_ammended.tmp.fcl; }' EXIT
+
+FILE=$1
+NEVTS=$2
+
+if [ ! -f $FILE ]; then
+  FILE=${PWD}/${FILE}
+  if [ ! -f $FILE ]; then
+    echo "File not valid"
+    exit 0
+  fi
+fi
+
+MAXEVTS=$(echo "std::cout << ND_depos_packets->GetEntries() << std::endl;" | root -l -b "$FILE" 2>/dev/null | tail -1)
+
+if [ -z $NEVTS ] || [ $NEVTS -eq -1 ]; then
+  NEVTS=$MAXEVTS
+elif [ $NEVTS -gt $MAXEVTS ]; then
+  echo "Not enough events in $FILE, running over the max $MAXEVTS"
+  NEVTS=$MAXEVTS
+fi
+
+sed "s/physics\.producers\.IonAndScint\.NDDataLoc:.*/physics\.producers\.IonAndScint\.NDDataLoc: \"${FILE}\"/" ${MRB_TOP}/srcs/duneextrapolation/duneextrapolation/MyNDFDTranslation/run_fcls/run_LoadNDData.fcl > /tmp/run_LoadNDData_ammended.tmp.fcl
+
+lar -c /tmp/run_LoadNDData_ammended.tmp.fcl -n $NEVTS -o test.root
