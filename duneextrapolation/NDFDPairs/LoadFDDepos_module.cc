3f3330b0e3ef39a1b93040b7834b1870ba9b1f39
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2023-11-27 16:39:50 -0600

    use flag on fd_deps added at larnd-sim stage to know which depos to drop for det resp pairs

--- duneextrapolation/NDFDPairs/LoadFDDepos_module.cc
+++ duneextrapolation/NDFDPairs/LoadFDDepos_module.cc
@@ -34,6 +34,7 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <math.h>
 
 typedef struct depo {
   int eventID;
@@ -53,6 +54,7 @@ typedef struct depo {
   double dx;
   double dEdx;
   double dE;
+  int outsideNDLAr;
 } depo;
 
 HighFive::CompoundType make_depo() {
@@ -73,69 +75,8 @@ HighFive::CompoundType make_depo() {
     {"t0", HighFive::AtomicType<double>{}},
     {"dx", HighFive::AtomicType<double>{}},
     {"dEdx", HighFive::AtomicType<double>{}},
-    {"dE", HighFive::AtomicType<double>{}}
-  };
-}
-
-typedef struct track { // ND-LAr depo have different structure because of larnd-sim
-  int eventID;
-  int trackID;
-  int uniqID;
-  int pdgID;
-  double x_end;
-  double x_start;
-  double y_end;
-  double y_start;
-  double z_end;
-  double z_start;
-  double x;
-  double y;
-  double z;
-  double t_start;
-  double t_end;
-  double t;
-  double t0_end;
-  double t0_start;
-  double t0;
-  int n_electrons;
-  int n_photons;
-  double tran_diff;
-  double long_diff;
-  double dx;
-  double dEdx;
-  double dE;
-  int pixel_plane;
-} track;
-
-HighFive::CompoundType make_track() {
-  return {
-    {"eventID", HighFive::AtomicType<int>{}},
-    {"trackID", HighFive::AtomicType<int>{}},
-    {"uniqID", HighFive::AtomicType<int>{}},
-    {"pdgID", HighFive::AtomicType<int>{}},
-    {"x_end", HighFive::AtomicType<double>{}},
-    {"x_start", HighFive::AtomicType<double>{}},
-    {"y_end", HighFive::AtomicType<double>{}},
-    {"y_start", HighFive::AtomicType<double>{}},
-    {"z_end", HighFive::AtomicType<double>{}},
-    {"z_start", HighFive::AtomicType<double>{}},
-    {"x", HighFive::AtomicType<double>{}},
-    {"y", HighFive::AtomicType<double>{}},
-    {"z", HighFive::AtomicType<double>{}},
-    {"t_start", HighFive::AtomicType<double>{}},
-    {"t_end", HighFive::AtomicType<double>{}},
-    {"t", HighFive::AtomicType<double>{}},
-    {"t0_end", HighFive::AtomicType<double>{}},
-    {"t0_end", HighFive::AtomicType<double>{}},
-    {"t0", HighFive::AtomicType<double>{}},
-    {"n_electrons", HighFive::AtomicType<int>{}},
-    {"n_photons", HighFive::AtomicType<int>{}},
-    {"tran_diff", HighFive::AtomicType<double>{}},
-    {"long_diff", HighFive::AtomicType<double>{}},
-    {"dx", HighFive::AtomicType<double>{}},
-    {"dEdx", HighFive::AtomicType<double>{}},
     {"dE", HighFive::AtomicType<double>{}},
-    {"pixel_plane", HighFive::AtomicType<int>{}}
+    {"outsideNDLAr", HighFive::AtomicType<int>{}}
   };
 }
 
@@ -156,7 +97,6 @@ HighFive::CompoundType make_vertex() {
 }
 
 HIGHFIVE_REGISTER_TYPE(depo, make_depo)
-HIGHFIVE_REGISTER_TYPE(track, make_track)
 HIGHFIVE_REGISTER_TYPE(vertex, make_vertex)
 
 namespace extrapolation {
@@ -192,18 +132,19 @@ public:
 private:
   HighFive::File* fFile;
   std::map<int, std::vector<depo>> fDepos;
-  std::map<int, std::set<int>> fNDDriftedUniqIDs;
   std::vector<int> fEventIDs;
 
   std::string fNDFDH5FileLoc;
 
-  bool fNDDriftedOnly;
+  bool fInsideNDOnly;
+  double fTargetSEDLen;
 };
 
 extrapolation::LoadFDDepos::LoadFDDepos(fhicl::ParameterSet const& p)
   : EDProducer{p},
     fNDFDH5FileLoc (p.get<std::string>("NDFDH5FileLoc")),
-    fNDDriftedOnly (p.get<bool>("NDDriftedOnly"))
+    fInsideNDOnly (p.get<bool>("InsideNDOnly")),
+    fTargetSEDLen  (p.get<double>("TargetSEDLen"))
 {
   produces<std::vector<sim::SimEnergyDeposit>>("LArG4DetectorServicevolTPCActive");
   produces<std::vector<sim::SimEnergyDeposit>>("eventID");
@@ -219,19 +160,24 @@ void extrapolation::LoadFDDepos::produce(art::Event& e)
   int currEventID = fEventIDs.back();
   fEventIDs.pop_back();
   const std::vector<depo> eventDepos = fDepos[currEventID];
-  const std::set<int> eventNDDriftedUniqIDs = fNDDriftedUniqIDs[currEventID];
 
   std::cout << "eventID = " << currEventID << ": Loading " << eventDepos.size() << " depos";
-  if (fNDDriftedOnly){
-    std::cout << " (dropping " << eventDepos.size() - eventNDDriftedUniqIDs.size() << ")";
+  if (fInsideNDOnly){
+    int numDropping = 0;
+    for (const depo dep : eventDepos) {
+      if (dep.outsideNDLAr == 1) {
+        numDropping++;
+      }
+    }
+    std::cout << " (dropping " << numDropping << ")";
   }
   std::cout << "\n";
 
   // SED vector with single SED that stores the eventID for matching with ND data later
   // eventID is being stored in the trackID member
   auto evNum = std::make_unique<std::vector<sim::SimEnergyDeposit>>();
-  geo::Point_t posStart = geo::Point_t(0,0,0);
-  geo::Point_t posEnd = geo::Point_t(0,0,0);
+  geo::Point_t posStart = geo::Point_t(0, 0, 0);
+  geo::Point_t posEnd = geo::Point_t(0, 0, 0);
   sim::SimEnergyDeposit ID = sim::SimEnergyDeposit(
     0, 0, 0, 0, posStart, posEnd, 0, 0, currEventID, 0
   );
@@ -239,15 +185,46 @@ void extrapolation::LoadFDDepos::produce(art::Event& e)
 
   auto SEDs = std::make_unique<std::vector<sim::SimEnergyDeposit>>();
   for (const depo dep : eventDepos) {
-    if (fNDDriftedOnly && !eventNDDriftedUniqIDs.count(dep.uniqID)) {
+    if (fInsideNDOnly && dep.outsideNDLAr == 1) {
       continue;
     }
-    geo::Point_t posStart(dep.x_start, dep.y_start, dep.z_start);
-    geo::Point_t posEnd(dep.x_end, dep.y_end, dep.z_end);
-    sim::SimEnergyDeposit SED = sim::SimEnergyDeposit(
-      0, 0, 0, dep.dE, posStart, posEnd, dep.t0_start, dep.t0_end, 1, 1
-    );
-    SEDs->push_back(SED);
+    if (!fTargetSEDLen) { // Load as an SED
+      const geo::Point_t posStart(dep.x_start, dep.y_start, dep.z_start);
+      const geo::Point_t posEnd(dep.x_end, dep.y_end, dep.z_end);
+      sim::SimEnergyDeposit SED = sim::SimEnergyDeposit(
+        0, 0, 0, dep.dE, posStart, posEnd, dep.t0_start, dep.t0_end, 1, 1
+      );
+      SEDs->push_back(SED);
+    }
+    else { // Break into steps and load each as an SED
+      const int nSteps = std::ceil(dep.dx / fTargetSEDLen);
+      const double deltaX = (dep.x_end - dep.x_start) / (double)nSteps;
+      const double deltaY = (dep.y_end - dep.y_start) / (double)nSteps;
+      const double deltaZ = (dep.z_end - dep.z_start) / (double)nSteps;
+      const double deltaT0 = (dep.t0_end - dep.t0_start) / (double)nSteps;
+      for (int iStep = 0; iStep < nSteps; iStep++) {
+        const geo::Point_t posStart(
+          dep.x_start + iStep * deltaX,
+          dep.y_start + iStep * deltaY,
+          dep.z_start + iStep * deltaZ
+        );
+        const geo::Point_t posEnd(
+          dep.x_start + (iStep + 1) * deltaX,
+          dep.y_start + (iStep + 1) * deltaY,
+          dep.z_start + (iStep + 1) * deltaZ
+        );
+        // Ideally would account for fluctuations in energy between steps, not possible without
+        // running edep-sim with small step size which makes larnd-sim behave weird
+        sim::SimEnergyDeposit SED = sim::SimEnergyDeposit(
+          0, 0, 0,
+          dep.dE / (double)nSteps,
+          posStart, posEnd,
+          dep.t0_start + iStep * deltaT0, dep.t0_start + (iStep + 1) * deltaT0,
+          1, 1
+        );
+        SEDs->push_back(SED);
+      }
+    }
   }
 
   e.put(std::move(SEDs), "LArG4DetectorServicevolTPCActive");
@@ -266,18 +243,10 @@ void extrapolation::LoadFDDepos::beginJob()
   std::vector<depo> readDepos;
   HighFive::DataSet datasetDepos = fFile->getDataSet("fd_deps");
   datasetDepos.read(readDepos);
-  std::vector<track> readTracks;
-  HighFive::DataSet datasetTracks = fFile->getDataSet("tracks");
-  datasetTracks.read(readTracks);
 
   // Create maps eventIDs and data
   for (const depo dep : readDepos) {
     fDepos[dep.eventID].push_back(dep);
-  }
-  for (const track t : readTracks) {
-    if (t.pixel_plane != -1) {
-      fNDDriftedUniqIDs[t.eventID].insert(t.uniqID);
-    }
   }
 
   for (const vertex vtx : readVtx) {
