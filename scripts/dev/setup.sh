e19dc6202d7e3b1a2e17a7f586b3451d647bfd8d
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-03-06 17:33:26 -0600

    upate script

--- scripts/dev/setup.sh
+++ scripts/dev/setup.sh
@@ -1,5 +1,5 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-source /exp/dune/app/users/awilkins/duneextrapolation_larsoft_updated_upsdeps/localProducts_larsoft_v09_78_04_e20_prof/setup
+source /exp/dune/app/users/awilkins/duneextrapolation_larsoft_updated_upsdeps/localProducts_larsoft_v09_78_03_e20_prof/setup
 setup dunesw v09_78_03d01 -q e20:prof
 setup duneprototypes v09_78_03d01 -q e20:prof
 setup duneana v09_78_03d01 -q e20:prof
