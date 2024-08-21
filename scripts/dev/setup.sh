1d8e38554a04c65ab2aa2a04a56b6cfca45c9022
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-08-21 12:06:25 -0500

    updates to fix build

--- scripts/dev/setup.sh
+++ scripts/dev/setup.sh
@@ -1,3 +1,6 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 source /exp/dune/app/users/awilkins/duneextrapolation_larsoft_updated_upsdeps/localProducts_larsoft_v09_78_04_e20_prof/setup
+setup dunesw v09_78_03d01 -q e20:prof
+setup dunedataprep v09_78_03d01 -q e20:prof
+setup dunecore v09_78_03d01 -q e20:prof
 mrbslp
