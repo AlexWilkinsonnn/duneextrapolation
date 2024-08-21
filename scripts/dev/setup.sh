e6e01b85ab3652bd7fe4593f918eaeb3eb27153a
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-08-21 11:11:32 -0500

    setting up these products is just breaking the build, maybe I needed them for when I was developing larsim alongside for the tdr branch?

--- scripts/dev/setup.sh
+++ scripts/dev/setup.sh
@@ -1,9 +1,3 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 source /exp/dune/app/users/awilkins/duneextrapolation_larsoft_updated_upsdeps/localProducts_larsoft_v09_78_04_e20_prof/setup
-setup dunesw v09_78_03d01 -q e20:prof
-setup duneprototypes v09_78_03d01 -q e20:prof
-setup duneana v09_78_03d01 -q e20:prof
-setup dunesim v09_78_03d01 -q e20:prof
-setup dunecore v09_78_03d01 -q e20:prof
 mrbslp
-
